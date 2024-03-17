68a37619ba1c753f82cc87524c222706a4bd977c
bongjoonkim <bongjoonkim96@gmail.com>
2024-03-17 11:40:03 +0900

    렌더링 테스트 로직 추가

---
+++ src/code1/Code.tsx
@@ -0,0 +1,44 @@
+import {memo, useEffect, useState} from "react";
+
+interface Props {
+  counter : number;
+}
+
+const Component = memo((props : Props) => {
+  useEffect(() => {
+    console.log("Component has been rendered!");
+  })
+  return (
+    <h1>{props.counter}</h1>
+  )
+});
+
+interface DeeperProps {
+  counter : {
+    counter : number;
+  }
+}
+
+const DeepterComponent = memo((props: DeeperProps) => {
+  useEffect(() => {
+    console.log("DeepterComponent has been rendered");
+  })
+  
+  return (
+    <h1>{props.counter.counter}</h1>
+  )
+})
+function Code(props : any) {
+  const [_, setCounter] = useState(0);
+  const handleClick = () => {
+    setCounter(prev => prev + 1);
+  }
+  return (
+    <>
+      <Component counter={100} />
+      <DeepterComponent counter={{counter : 100}} />
+    </>
+  )
+}
+
+export default Code;
